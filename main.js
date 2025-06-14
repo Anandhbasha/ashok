499d27314f042c9bbf202ba540cbeb5c0ade9ddc
Anandhbasha <anadhakm@gmail.com>
2025-06-14 16:17:26 +0530

    first class

---
+++ main.js
@@ -0,0 +1,88 @@
+// // //Hoisting
+// //     //var
+// //     console.log(x);
+    
+// //     var x = 20;
+// //     // console.log(x);
+// //     var x = 30;
+// //     x=50
+// //     // console.log(x);
+    
+    
+// //     //assignment Opeartor =
+// //     //x = variable name
+// //     //var= delcalration
+// //     //let 
+// //     // console.log(a);
+// //     let a = 50;
+// //     // let a=60;
+    
+    
+// //     //const
+// //     const c = 20;
+// //     c= 50
+//     // console.log(c);
+    
+//     //scope variable
+//     //global variable
+//     {
+//         let d= 70
+//     }
+//     // console.log(d);
+//     {
+//         var x = 80
+//     }
+//     console.log(x);
+//     {
+//         const value = 42;
+//     }
+//     console.log(value);
+// Opeartors
+    //arithmetic
+        // +,-,*,/,%,++,--
+        let a= 10
+        let b=5
+        console.log(a+b);
+        console.log(a-b);
+        console.log(a*b);
+        console.log(a/b);
+        console.log(a%6);
+        // a= a+1
+        // a++
+        console.log(a++);
+        console.log(a);
+        console.log(++a);
+        console.log(--b);
+        b--
+        console.log(b--)    
+    //conditonal
+        // <,>,<=,>=,==,===
+        console.log(b>a);
+        console.log(b<a);
+        console.log(b<=3);
+        console.log(a>=14);
+        console.log(a=='12');//data type not compare
+        console.log(a==="12");
+
+        // console.log(a,b);
+        
+    //logical opeartor
+        // && , || ,!
+        if(b<a && a===12 && b==3){
+            console.log("And is working");
+            
+        }
+        // Or Operator
+        if(b>a || a===14 || b==3){
+            console.log("Or is working");
+            
+        }
+        console.log(a!=b);
+        
+
+    //Ternary opeartor
+        let age = 17
+        console.log(age>=18?"Adult":"Teenager");
+        
+    
+    
